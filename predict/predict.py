5a266b8c3fa8cefebc9e25cfdb28915f1ea7c583
Jonathan Han <87393036+Jona-Han@users.noreply.github.com>
2024-03-23 16:38:54 -0700

    Write code in predict to load models with pickle and make a prediction

--- predict/predict.py
+++ predict/predict.py
@@ -2,12 +2,27 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pickle
 
-def predict(self, input_sample, input_models):
-    for model in input_models:
-        with open(model, 'wb') as f:
-            pickle.loads()
+"""
+Returns one prediction.
 
-    clf = pickle.loads(input_model)
-    input_array = np.array(input_sample)
-    prediction = clf.predict(input_array)
-    return prediction
+TODO: Update predict to return a tuple/dict/array of predictions for 1 frame
+
+input_models = list of model file names
+"""
+
+
+def predict(self, input_sample, input_models):
+    # Load models from string names
+    sklearn_models = []
+    for model in input_models:
+        try:
+            with open(model, "rb") as f:
+                sklearn_models.append(pickle.load(f))
+        except IOError:
+            print("failed to open model:" + model)
+
+    # Go through models
+    for model in sklearn_models:
+        input_array = np.array(input_sample)
+        prediction = model.predict(input_array)
+        return prediction
