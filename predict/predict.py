6cd8925e95b52f2832d51ea75d72ad459c7c0c73
Jonathan Han <87393036+Jona-Han@users.noreply.github.com>
2024-03-23 16:07:56 -0700

    Add example openpose code to main.py

--- predict/predict.py
+++ predict/predict.py
@@ -2,8 +2,12 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pickle
 
-def predict(self, input_sample, input_file):
-    clf = pickle.loads(input_file)
+def predict(self, input_sample, input_models):
+    for model in input_models:
+        with open(model, 'wb') as f:
+            pickle.loads()
+
+    clf = pickle.loads(input_model)
     input_array = np.array(input_sample)
     prediction = clf.predict(input_array)
     return prediction
