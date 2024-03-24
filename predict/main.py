65a90f67b977ebb51d607935aefaf828180ec604
Jonathan Han <87393036+Jona-Han@users.noreply.github.com>
2024-03-23 19:32:56 -0700

    Modified mediapipe code to synchronously determine poses

--- predict/main.py
+++ predict/main.py
@@ -5,10 +5,6 @@ import cv2
 import numpy as np
 from predict.Predictor import Predictor
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-from mediapipe import solutions
-from mediapipe.framework.formats import landmark_pb2
 
 model_path = '../models/pose_landmarker_heavy.task'
 
@@ -16,64 +12,42 @@ model_file_names = [
     "test.pkl"
 ]
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-  pose_landmarks_list = detection_result.pose_landmarks
-  annotated_image = np.copy(rgb_image)
-
-  # Loop through the detected poses to visualize.
-  for idx in range(len(pose_landmarks_list)):
-    pose_landmarks = pose_landmarks_list[idx]
-
-    # Draw the pose landmarks.
-    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    pose_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      pose_landmarks_proto,
-      solutions.pose.POSE_CONNECTIONS,
-      solutions.drawing_styles.get_default_pose_landmarks_style())
-  return annotated_image
-
-
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 def main():
-    BaseOptions = mp.tasks.BaseOptions
-    PoseLandmarker = mp.tasks.vision.PoseLandmarker
-    PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-    PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
-    VisionRunningMode = mp.tasks.vision.RunningMode
-
-    # Create a pose landmarker instance with the live stream mode:
-    def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
-        # annotated_image = draw_landmarks_on_image(output_image.numpy_view(), result)
-    #     cv2.imshow('Show', annotated_image)
-    #     cv2_imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
-        print('pose landmarker result: {}'.format(result))
-
-    options = PoseLandmarkerOptions(
-        base_options=BaseOptions(model_asset_path=model_path),
-        running_mode=VisionRunningMode.LIVE_STREAM,
-        result_callback=print_result)
-
     vid = cv2.VideoCapture(1)
-    with PoseLandmarker.create_from_options(options) as landmarker:
-        while True:
-            ret, frame = vid.read()
+    with mp_pose.Pose(
+            min_tracking_confidence=0.5,
+            min_detection_confidence=0.5,
+            model_complexity=1,
+            smooth_landmarks=True,
+    ) as pose:
+        while vid.isOpened():
+            # read webcam image
+            success, image = vid.read()
 
-            # the 'q' button is set as the quitting button you may use any desired button of your choice
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                return
+            # skip empty frames
+            if not success:
+                continue
 
-            # Convert the frame received from OpenCV to a MediaPipe’s Image object.
-            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-            # The results are accessible via the `result_callback` provided in the `PoseLandmarkerOptions` object.
-            # The pose landmarker must be created with the live stream mode.
-            frame_timestamp_ms = int(time.time()*1000.0)
-            landmarker.detect_async(mp_image, frame_timestamp_ms)
+            # calculate pose
+            results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-            # Display the resulting frame
-            cv2.imshow('frame', frame)
+            # draw 3D pose landmarks live
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+
+            print('pose landmarker result: {}'.format(results.pose_landmarks))
+
+            # draw image
+            cv2.imshow("MediaPipePose", cv2.flip(image, 1))
+
+            if cv2.waitKey(5) & 0xFF == ord('q'):
+                break
 
     # After the loop release the cap object
     vid.release()
