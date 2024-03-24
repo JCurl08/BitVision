6dac87c0d081d473ab006369d3ba0171c6b1e2f8
Jonathan Han <87393036+Jona-Han@users.noreply.github.com>
2024-03-23 18:50:25 -0700

    Fixed to successfully put images into mediapipe

--- predict/main.py
+++ predict/main.py
@@ -10,7 +10,7 @@ from mediapipe.tasks.python import vision
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 
-model_path = '/models/pose_landmarker_heavy.task'
+model_path = '../models/pose_landmarker_heavy.task'
 
 model_file_names = [
     "test.pkl"
@@ -46,11 +46,10 @@ def main():
 
     # Create a pose landmarker instance with the live stream mode:
     def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
-        annotated_image = draw_landmarks_on_image(output_image.numpy_view(), result)
-        cv2.imshow('frame', annotated_image)
+        # annotated_image = draw_landmarks_on_image(output_image.numpy_view(), result)
+    #     cv2.imshow('Show', annotated_image)
+    #     cv2_imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
         print('pose landmarker result: {}'.format(result))
-        # imright = output_image.numpy_view()
-        # cv2.imwrite('somefile.jpg', imright)
 
     options = PoseLandmarkerOptions(
         base_options=BaseOptions(model_asset_path=model_path),
@@ -59,21 +58,22 @@ def main():
 
     vid = cv2.VideoCapture(1)
     with PoseLandmarker.create_from_options(options) as landmarker:
-        ret, frame = vid.read()
+        while True:
+            ret, frame = vid.read()
 
-        # the 'q' button is set as the quitting button you may use any desired button of your choice
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            return
+            # the 'q' button is set as the quitting button you may use any desired button of your choice
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                return
 
-        # Convert the frame received from OpenCV to a MediaPipe’s Image object.
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-        # The results are accessible via the `result_callback` provided in the `PoseLandmarkerOptions` object.
-        # The pose landmarker must be created with the live stream mode.
-        frame_timestamp_ms = int(time.perf_counter())
-        landmarker.detect_async(mp_image, frame_timestamp_ms)
+            # Convert the frame received from OpenCV to a MediaPipe’s Image object.
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+            # The results are accessible via the `result_callback` provided in the `PoseLandmarkerOptions` object.
+            # The pose landmarker must be created with the live stream mode.
+            frame_timestamp_ms = int(time.time()*1000.0)
+            landmarker.detect_async(mp_image, frame_timestamp_ms)
 
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
+            # Display the resulting frame
+            cv2.imshow('frame', frame)
 
     # After the loop release the cap object
     vid.release()
